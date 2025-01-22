58c08b13df18bb5ada76936693ca3418a6126d72
Muhammad Usman <154560114+Musma-n@users.noreply.github.com>
2025-01-23 03:51:18 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const todos = [];
 
 app.use(express.json()); // To convert body into JSON
 app.use(
-  cors({ origin: ["http://localhost:5173","todo-list-with-backend.netlify.app/"] }),
+  cors({ origin: ["http://localhost:5173","https://todolistwithbackend.netlify.app"] }),
 );
 
 app.get("/api/v1/todos", (request, response) => {
